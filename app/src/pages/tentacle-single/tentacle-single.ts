d43ba81d81c47b8fd1851a1a40e3dbf70a3c4acb
Florian Aumeier <flo.aumeier@gmail.com>
2017-05-07 11:53:08 +0200

    feat: detail tentacle view TODO: make dynamic

--- app/src/pages/tentacle-single/tentacle-single.ts
+++ app/src/pages/tentacle-single/tentacle-single.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TentacleService } from '../../providers/tentacle-service';
 
 /**
  * Generated class for the TentacleSingle page.
@@ -11,14 +12,50 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-tentacle-single',
   templateUrl: 'tentacle-single.html',
+  providers: [TentacleService]
 })
 export class TentacleSingle {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	public foundTentacles;
+	public currentTentacle;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public tentacleService: TentacleService) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TentacleSingle');
+    console.log('tentacle-single started');
+    /*this.getTentacles();*/
+    this.getTentacleById(1);
+  }
+
+    getTentacles() {
+    this.tentacleService.getTentacles().subscribe(
+        data => {
+          this.foundTentacles = data.json();
+        },
+        err => console.error(err),
+        () => console.log('Tentacles called from tentacle-single: completed')
+    );
+  }
+
+  getTentacleById(id) {
+    this.tentacleService.getTentacleById(id).subscribe(
+        data => {
+          this.currentTentacle = data.json();
+        },
+        err => console.error('Fehler :(' + err),
+        () => console.log('Tentacle completed ' + id)
+    );
+  }
+
+    showSoilState(currentState) {
+    if(currentState > 0 && currentState < 0.3) {
+      return 'star-outline';
+    } else if(currentState > 0.3 && currentState < 0.6) {
+      return 'star-half';
+    } else {
+      return 'star';
+    }
   }
 
 }
