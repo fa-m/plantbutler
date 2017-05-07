5666ab86b9e426be22b8fe8d019fee03f358d7a7
fa-m <info@fmalik.de>
2017-05-07 04:16:44 +0200

    core: added Test API Call

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,19 +6,25 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import { Configuration } from '../pages/configuration/configuration';
+import { HttpTest } from '../pages/http-test/http-test';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { TentacleService } from '../providers/tentacle-service';
+import {HttpModule} from "@angular/http";
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     ListPage,
-    Configuration
+    Configuration,
+    HttpTest
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
@@ -26,11 +32,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     HomePage,
     ListPage,
-    Configuration
+    Configuration,
+    HttpTest
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    TentacleService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
